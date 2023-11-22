a7da105092a86760260ffbe7457ba3d0007dc9a4
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-22 12:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+  // Remove the '#' if present and convert to lowercase
+  shortHex = shortHex.replace(/^#/, '').toLowerCase();
 
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+  // Check if the input is a valid short hex code
+  if (shortHex.length === 3 && /^[0-9a-f]{3}$/.test(shortHex)) {
+    // Extend the short hex code to a full hex code
+    const fullHex = shortHex
+      .split('')
+      .map(char => char.repeat(2))
+      .join('');
+
+    // Add the '#' prefix and return the full hex code
+    return `#${fullHex}`;
+  } else {
+    // Invalid input, return null or handle accordingly
+    return null;
+  }
+}
+
+// Examples
+console.log(extendHex("#abc"));   // Output: "#aabbcc"
+console.log(extendHex("abc"));    // Output: "#aabbcc"
+console.log(extendHex("#AbC"));   // Output: "#AABBCC"
+console.log(extendHex("#f09"));   // Output: "#ff0099"
